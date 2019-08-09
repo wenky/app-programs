25c0e7139416b31d30c5069d283debb991bdd1d3
unknown <uiteam2018@alevartech.com>
2019-08-09 06:04:48 +0530

    initial commit

--- exercises/src/com/wenky/multithreading/PrintNumber.java
+++ exercises/src/com/wenky/multithreading/PrintNumber.java
@@ -1,4 +1,32 @@
 package com.wenky.multithreading;
 
-public class PrintNumber {
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class PrintNumber extends Thread{
+    Predicate<Integer> predicate;
+    CyclicBarrier cyclicBarrier;
+    int n;
+    PrintNumber(Predicate<Integer> predicate, CyclicBarrier cyclicBarrier,int n){
+        this.predicate=predicate;
+        this.n=n;
+        this.cyclicBarrier=cyclicBarrier;
+    }
+
+    @Override
+    public void run() {
+        int i=0;
+        try {
+            do {
+                i=i+1;
+                if (this.predicate.test(i))
+                    System.out.println(getName()+"-->"+i);
+                cyclicBarrier.await();
+            }while (i <n);
+        }catch (InterruptedException |BrokenBarrierException e){
+            e.printStackTrace();
+        }
+    }
 }
--- exercises/src/com/wenky/multithreading/PrintNumbers.java
+++ exercises/src/com/wenky/multithreading/PrintNumbers.java
@@ -1,4 +1,15 @@
 package com.wenky.multithreading;
 
+import java.util.concurrent.CyclicBarrier;
+
 public class PrintNumbers {
+    public static void main(String[] args) {
+        CyclicBarrier cyclicBarrier=new CyclicBarrier(2);
+        PrintNumber oddThread=new PrintNumber(n->n%2!=0,cyclicBarrier,10);
+        PrintNumber evenThread=new PrintNumber(n->n%2==0,cyclicBarrier,10);
+        oddThread.setName("Odd Thread");
+        evenThread.setName("Even Thread");
+        oddThread.start();
+        evenThread.start();
+    }
 }
